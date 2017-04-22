23e2bff82e2ae362eff3f24bf16c968e7bca13c9
Rui Peres <sauron.is.alive@gmail.com>
2017-04-22 22:23:41 +0100

    Added the lazy version

--- eager_lazy.exs
+++ eager_lazy.exs
@@ -2,11 +2,16 @@ defmodule EagerLazy  do
   def odd?(x) do
       rem(x, 2) == 0
   end
-
   def pipeMeUp(x) do
     1..x
     |> Enum.map(&(&1 + 3))
     |> Enum.filter(&odd?/1)
     |> Enum.sum
+  end
+
+  def pipeMeUpLazyEdition(x) do
+    1..x
+    |> Stream.map(&(&1 + 3))
+    |> Stream.filter(&odd?/1)
   end
 end
