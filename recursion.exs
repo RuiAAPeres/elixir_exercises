3364d9411727caecb8ea6988ac89f1d919348bd4
Rui Peres <sauron.is.alive@gmail.com>
2017-04-22 19:34:12 +0100

    Not sure which one I prefer tbh

--- recursion.exs
+++ recursion.exs
@@ -3,9 +3,18 @@ defmodule Recursion do
   defp sum([head | tail], acc), do: sum(tail, acc + head)
   defp sum([], acc), do: acc
 
-  def fibo(0), do: 0
-  def fibo(1), do: 1
-  def fibo(arg1), do: fibo(arg1, 0, 1)
-  defp fibo(0, x, y), do: x + y
-  defp fibo(n, x, y), do: fibo(n - 1, y, x+y)
+  def fibo(n) do
+    case n do
+      0 -> 0
+      1 -> 1
+      _ -> fibo(n, 0, 1)
+    end
+  end
+
+  defp fibo(n, x, y) do
+    case n do
+      0 -> x
+      _ -> fibo(n - 1, y, x+y)
+    end
+  end
 end
