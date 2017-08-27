3680b1c36d35125d81828abc6548a59d53f86b67
Rui Peres <sauron.is.alive@gmail.com>
2017-08-27 15:37:32 +0100

    Ping the pong done

--- pingpong/lib/pingpong.ex
+++ pingpong/lib/pingpong.ex
@@ -1,6 +1,23 @@
 defmodule Pingpong do
 
-  def hello do
-    :world
+  def start do
+    first = spawn(Pingpong, :loop, [])
+    second = spawn(Pingpong, :loop, [])
+    send(first, {:ping, second})
+  end
+
+  def loop do
+      random_number = :rand.uniform(2) * 1000
+      receive do
+          {:ping, process} ->
+              :timer.sleep(random_number)
+              IO.puts "ping"
+              send(process, {:pong, self()})
+          {:pong, process} ->
+              :timer.sleep(random_number)
+              IO.puts "pong"
+              send(process, {:ping, self()})
+      end
+      loop()
   end
 end
